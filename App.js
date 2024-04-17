d43e5ab16c13adcad58af8fce3cdb062048d1c58
Harish Raj <harishraj.k2022@vitstudent.ac.in>
2024-04-17 21:02:29 +0530

    first commit

---
+++ App.js
@@ -0,0 +1,32 @@
+import React, { useState } from 'react';
+import { View, Text, Button, StyleSheet } from 'react-native';
+
+const App = () => {
+  const [backgroundColor, setBackgroundColor] = useState('#0088ff');
+
+  const changeBackgroundColor = () => {
+    setBackgroundColor(backgroundColor === '#0088ff' ? 'grey' : '#0088ff');
+  };
+
+  return (
+    <View style={[styles.container, { backgroundColor }]}>
+      <Text style={styles.text}>HarishRajK(22MIS1189)</Text>
+      <Button title="Change Background" onPress={changeBackgroundColor} />
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  text: {
+    fontSize: 20,
+    fontWeight: 'bold',
+    marginBottom: 20,
+    color: 'white',
+  },
+});
+export default App;
